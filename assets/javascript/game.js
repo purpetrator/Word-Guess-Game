8fbaac6bf8978aba894ee2761d64b6ee37db50c2
Ana Chernov <anastasia.chernov@gmail.com>
2019-10-16 22:23:01 -0400

    Refactored a lot of code and made the user experience better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,35 @@
 
 var myDogs = ["pomeranian", "beagle", "corgi", "vizsla", "greyhound", "husky"];
 
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
 var wins = 0;
 var remaining = 10;
 var guessedLetters = [];
@@ -23,47 +52,65 @@ window.onload = function() {
 
   // Loop over the chosen word
   for (var i = 0; i < chosenWord.length; i++) {
-    // Display every character at every index of chosenWord
+    // Log every character at every index of chosenWord
     console.log(chosenWord[i]);
     // Add a new item ("_") to the wordBlanks array
     wordBlanks.push("_");
     // Join the elements of the array into a string and display it on the screen
-    document.getElementById("the-word").textContent = wordBlanks.join(" ");
+    wordElement.textContent = wordBlanks.join(" ");
   }
 };
 
 // When user presses a key
 document.onkeydown = function(event) {
-  // If the entry exists within the alphabet array (must be a letter)
-  if (chosenWord.includes(event.key)) {
-    console.log(event.key);
-    var indexPosition = chosenWord.indexOf(event.key); // Or i could write a for-loop here to replace this
+  if (alphabet.includes(event.key)) {
+    // If the entry exists within the alphabet array (must be a letter)
+    if (chosenWord.includes(event.key)) {
+      console.log(event.key);
+      // Check to see how many times letter shows up in word
+      for (var i = 0; i < chosenWord.length; i++) {
+        if (chosenWord[i] === event.key) {
+          wordBlanks[i] = event.key;
+        }
+      }
+      wordElement.textContent = wordBlanks.join(" "); // and the for-loop would end here
 
-    wordBlanks[indexPosition] = event.key;
+      // If the entry DOES NOT exist within the alphabet array
+    } else {
+      // If it DOES NOT already exist in the guessed letters array (This prevents duplicate guessed letters)
+      if (!guessedLetters.includes(event.key)) {
+        remaining--;
 
-    document.getElementById("the-word").textContent = wordBlanks.join(" "); // and the for-loop would end here
+        // Add incorrectly guessed letters to guessedLetters array
+        guessedLetters.push(event.key);
+        // Join letters with a space
+        guessedAlready.textContent = guessedLetters.join(" ");
+      }
+    }
 
-    // If the entry DOES NOT exist within the alphabet array
-  } else {
-    remaining--; // How do I get this to stop at 0?
+    // Hide the directions
+    directionsText.textContent = "";
 
-    // Add incorrectly guessed letters to guessedLetters array
-    guessedLetters.push(event.key);
-    // Join letters with a space
-    document.getElementById("alreadyGuessed").textContent = guessedLetters.join(
-      " "
-    );
-  }
+    // Display the wins and guesses on the screen
+    guessesRemaining.textContent = " " + remaining;
 
-  // Hide the directions
-  directionsText.textContent = "";
+    // End game message
+    if (remaining == 0) {
+      console.log("you suck");
+      alert("Sorry! You're Out Of Guesses!");
+      confirm("Play again?", location.reload());
+    }
 
-  // Display the wins and guesses on the screen
-  guessesRemaining.textContent = " " + remaining;
-
-  if (remaining == 0) {
-    console.log("you suck");
-    alert("Sorry! You Lose!");
+    // Define what a win is and what happens when the word is guessed
+    if (!wordBlanks.includes("_")) {
+      wins++;
+      totalWins.textContent = " " + wins;
+      console.log("You Win!");
+      // Delays alert by half a second after increasing wins
+      setTimeout(function() {
+        alert("You Win!");
+      }, 500);
+    }
   }
 };
 
@@ -77,3 +124,6 @@ document.onkeydown = function(event) {
 // Once all _ are replaced with letters, user alerts 'You win'
 // When user wins, "wins" increases by one
 // When user wins, game resets to new
+
+// Odds and Ends:
+// * How do I reload game and keep cumulative wins?
